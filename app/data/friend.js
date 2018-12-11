4ec7fdce13a46a223295284d5d0169668de325dd
Vu Duong <vuduong191@gmail.com>
2018-12-10 23:42:05 -0800

    Adding image photo

--- app/data/friend.js
+++ app/data/friend.js
@@ -6,32 +6,32 @@ var friendArray = {
     data: [
     {
         name:"Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        photo:"http://static2.businessinsider.com/image/5899ffcf6e09a897008b5c04-1200/.jpg",
         scores:["5","1","4","4","5","1","2","5","4","1"]
     },
     {
         name:"Jacob Deming",
-        photo:"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        photo:"http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg",
         scores:["4","2","5","1","3","2","2","1","3","2"]
     },
     {
         name:"Jeremiah Scanlon",
-        photo:"https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        photo:"https://dp.profilepics.in/profile_pictures/stylish-boys/boys-profile-pics-806.jpg",
         scores:["5","2","2","2","4","1","3","2","5","5"]
     },
     {
         name:"Louis T. Delia",
-        photo:"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        photo:"https://blog.photofeeler.com/wp-content/uploads/2017/12/linkedin-profile-picture.jpg",
         scores:["3","3","4","2","2","1","3","2","2","3"]
     },
     {
         name:"Lou Ritter",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        photo:"https://www.trickscity.com/wp-content/uploads/2016/04/12356836_1666582430261394_7183559408635634317_o1-1020x1024.jpg",
         scores:["4","3","4","1","5","2","5","3","1","4"]
     },
     {
         name:"Jordan Biason",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        photo:"http://blogs.cuit.columbia.edu/asj2122/files/2013/07/profile.jpg",
         scores:["4","4","2","3","2","2","3","2","4","5"]
     },
 ]};
